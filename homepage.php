846a5a2352590cbf6d9b8807967ba5f7d7219684
lilykm27 <jdn3qj@virginia.edu>
2025-11-07 14:31:55 -0500

    changed tab title

--- homepage.php
+++ homepage.php
@@ -7,7 +7,7 @@ require('request-db.php');
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>NYT Bestsellers - Book Reviews</title>
+    <title>TopShelf</title>
     <style>
         * {
             margin: 0;
