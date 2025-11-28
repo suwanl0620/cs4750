08ccbb741c57fd7a8b1d64f77952657473093673
lilykm27 <jdn3qj@virginia.edu>
2025-11-28 11:33:35 -0500

    commented out sample data

--- my-reviews.php
+++ my-reviews.php
@@ -4,10 +4,46 @@ require('connect-db.php');
 require('reviews-db.php');
 // require some kind of reviews db 
 // should only be available if logged in
+/*
 $userID = $_SESSION['user_id'] ?? null;
 if (!$userID) { header("Location: login.php"); exit; }
 
 $reviews = getUserReviews($userID);
+*/
+$user_reviews = getUserReviews($_SESSION['user_id']);
+
+// sample data
+/*
+$user_reviews = [
+    [
+        'coverImage' => '',
+        'title' => 'To Kill A Mockingbird',
+        'author' => 'Lee, Harper',
+        'avgRating' => 4.26,
+        'userRating' => 5,
+        'review' => 'One of the required readings in high school but there is definitely a good reason for this. I will explain in the next few lines of this review, please keep reading. I think that it because it teaches you so much about life and allow you to explore outlying themes of empathy, prejudice, and small-town, to gain insights was human nature, and to understand how courage is demonstrated through action rather than physical strength',
+        'dateAdded' => 'Sep 12, 2025'
+    ],
+    [
+        'coverImage' => '',
+        'title' => 'One fish, two fish, red fish, blue fish',
+        'author' => 'Dr. Seuss',
+        'avgRating' => 4.57,
+        'userRating' => 5,
+        'review' => 'This book is exactly good. I love reading this book when I was young. I always told my mom to keep on reading. This fish, red fish, red fish, blue fish to them, so much...',
+        'dateAdded' => 'Apr 8, 2025'
+    ],
+    [
+        'coverImage' => '',
+        'title' => '90 Algorithms Every Programmer Should Know',
+        'author' => 'Ahmed, Imran',
+        'avgRating' => 4.11,
+        'userRating' => 4,
+        'review' => 'Mastering algorithms is a highly recommend. Aspiring computer scientists should understand the most popular algorithms every programmer should know thoroughly.',
+        'dateAdded' => 'Jan 5, 2025'
+    ]
+];
+*/
 ?>
 
  <!DOCTYPE html>
